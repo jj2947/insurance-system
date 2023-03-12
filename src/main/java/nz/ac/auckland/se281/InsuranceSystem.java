66a2e338930fadd52df7493f2393afe9f0a4cc80
Jessica Jiang <jessicaj2947@gmail.com>
2023-03-12 20:21:35 +1300

    modified printDatabase so the database prints correctly for all cases

--- src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
+++ src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
@@ -7,19 +7,33 @@ import java.util.ArrayList;
 public class InsuranceSystem {
 
   // Creates the arraylist that will store all the user profiles
-  private ArrayList<String> profiles = new ArrayList<String>();
+  private ArrayList<String> userNames = new ArrayList<String>();
+
+  private ArrayList<String> ages = new ArrayList<String>();
 
   public InsuranceSystem() {
     // Only this constructor can be used (if you need to initialise fields).
   }
 
   public void printDatabase() {
-    Database database = new Database();
-    database.printDB();
+    if (userNames.size() == 0) {
+      MessageCli.PRINT_DB_POLICY_COUNT.printMessage("0", "s", ".");
+    } else if (userNames.size() == 1) {
+      MessageCli.PRINT_DB_POLICY_COUNT.printMessage("1", ":");
+      MessageCli.PRINT_DB_PROFILE_HEADER_MINIMAL.printMessage("1", userNames.get(0), ages.get(0));
+    } else {
+      MessageCli.PRINT_DB_POLICY_COUNT.printMessage(String.valueOf(userNames.size()), "s", ":");
+      for (int i = 0; i < userNames.size(); i++) {
+        MessageCli.PRINT_DB_PROFILE_HEADER_MINIMAL.printMessage(String.valueOf(i + 1), userNames.get(i),
+            ages.get(i));
+      }
+    }
   }
 
   public void createNewProfile(String userName, String age) {
-    // TODO: Complete this method.
+    userNames.add(userName);
+    ages.add(age);
+    System.out.println("New profile created for " + userName + " with age " + age + ".");
   }
 
   public void loadProfile(String userName) {
