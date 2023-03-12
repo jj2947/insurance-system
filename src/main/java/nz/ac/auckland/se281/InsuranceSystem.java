2033f2adb62a5b894d002c76a2790a4cdad40195
Jessica Jiang <jessicaj2947@gmail.com>
2023-03-12 18:34:43 +1300

    created a new Database class and set up some initial code for the PRINT_DB command

--- src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
+++ src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
@@ -2,13 +2,21 @@ package nz.ac.auckland.se281;
 
 import nz.ac.auckland.se281.Main.PolicyType;
 
+import java.util.ArrayList;
+
 public class InsuranceSystem {
+
+  // Creates the arraylist that will store all the user profiles
+  private ArrayList<String> profiles = new ArrayList<String>();
+
   public InsuranceSystem() {
     // Only this constructor can be used (if you need to initialise fields).
   }
 
   public void printDatabase() {
-    // TODO: Complete this method.
+    if (profiles.size() == 0) {
+      System.out.println("Database has 0 profiles.");
+    }
   }
 
   public void createNewProfile(String userName, String age) {
