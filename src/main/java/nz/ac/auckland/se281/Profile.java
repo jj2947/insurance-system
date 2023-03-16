69e832714beed67a957ba3c6dea3946d3a3f6567
Jessica Jiang <jessicaj2947@gmail.com>
2023-03-16 17:03:33 +1300

    added some methods to Profile class

--- src/main/java/nz/ac/auckland/se281/Profile.java
+++ src/main/java/nz/ac/auckland/se281/Profile.java
@@ -5,5 +5,16 @@ public class Profile {
   String name;
   int age;
 
-  public Profile() {}
+  public Profile(String name, String age) {
+    this.name = name;
+    this.age = Integer.valueOf(age);
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public int getAge() {
+    return age;
+  }
 }
