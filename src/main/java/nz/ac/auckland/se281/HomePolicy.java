4c5bf8868961a6f08c86034be53943cc92be6e40
Jessica Jiang <jessicaj2947@gmail.com>
2023-04-11 14:02:11 +1200

    added constructor to HomePolicy

--- src/main/java/nz/ac/auckland/se281/HomePolicy.java
+++ src/main/java/nz/ac/auckland/se281/HomePolicy.java
@@ -1,5 +1,18 @@
 package nz.ac.auckland.se281;
 
 public class HomePolicy extends Policy {
-    
+
+  private String address;
+  private boolean rental;
+
+  public HomePolicy(String[] options) {
+    super(options);
+    address = options[1];
+
+    if (options[2].equals("yes")) {
+      rental = true;
+    } else {
+      rental = false;
+    }
+  }
 }
