72793c1bcf812e8960684c3a44d4896c1df0a400
Jessica Jiang <jessicaj2947@gmail.com>
2023-04-11 14:36:18 +1200

    added constructor to CarPolicy class

--- src/main/java/nz/ac/auckland/se281/CarPolicy.java
+++ src/main/java/nz/ac/auckland/se281/CarPolicy.java
@@ -1,5 +1,25 @@
 package nz.ac.auckland.se281;
 
 public class CarPolicy extends Policy{
+
+    private String makeAndModel;
+    private String licensePlate;
+    private boolean mechanicalBreakdown;
+
+    public CarPolicy(String[] options) {
+        super(options);
+
+        makeAndModel = options[1];
+        licensePlate = options[2];
+
+        if (options[3].equals("yes")) {
+        mechanicalBreakdown = true;
+        } else {
+            mechanicalBreakdown = false;
+        }
+
+    }
+
+    
     
 }
