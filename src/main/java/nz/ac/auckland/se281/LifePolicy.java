a9a523c76a1f63d284e01ca057d8b237ddee9c10
Jessica Jiang <jessicaj2947@gmail.com>
2023-04-11 14:26:54 +1200

    added constructor, and override of getBasePremium method to LifePolicy class

--- src/main/java/nz/ac/auckland/se281/LifePolicy.java
+++ src/main/java/nz/ac/auckland/se281/LifePolicy.java
@@ -1,5 +1,15 @@
 package nz.ac.auckland.se281;
 
 public class LifePolicy extends Policy {
-    
+
+  private int age;
+
+  public LifePolicy(String[] options, int age) {
+    super(options);
+  }
+
+  @Override
+  public int getBasePremium() {
+    return ((1 + age / 100) / 100) * sumInusred;
+  }
 }
