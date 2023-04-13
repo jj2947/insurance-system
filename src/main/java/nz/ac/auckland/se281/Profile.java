45d8d3aac967723b6089f85ba1ede0336786a2b1
Jessica Jiang <jessicaj2947@gmail.com>
2023-04-13 14:20:05 +1200

    added comments

--- src/main/java/nz/ac/auckland/se281/Profile.java
+++ src/main/java/nz/ac/auckland/se281/Profile.java
@@ -10,9 +10,11 @@ public class Profile {
   private String userName;
   private String age;
   private boolean loadStatus;
+
+  // Creates an ArrayList of Policy objects that will store the policies a profile has
   private ArrayList<Policy> policies = new ArrayList<>();
 
-  // Constructor which initialises userName and age vaariables
+  // Constructor
   public Profile(String userName, String age, boolean loadStatus) {
     this.userName = userName;
     this.age = age;
@@ -39,57 +41,76 @@ public class Profile {
     this.loadStatus = loadStatus;
   }
 
+  // Returns the number of policies in a profile
   public int getNumPolicies() {
     return policies.size();
   }
 
+  // Adds a policy to a profile
   public void addPolicy(Policy policy) {
     policies.add(policy);
   }
 
+  // Returns the policies of a profile
   public ArrayList<Policy> getPolicies() {
     return policies;
   }
 
+  // Returns the total price for the policies of a profile
   public int getTotalPrice() {
     int total = 0;
+
+    // Loops through the policies ArrayList and adds the discounted premium of each policy to the
+    // total price
     for (Policy policy : policies) {
       total += policy.getDiscountedPremium(policies.size());
     }
     return total;
   }
 
+  // Prints all the policies of a profile
   public void printPolicies() {
+
+    // Loops through the policies ArrayList, which contains all the policies a profile has
     for (Policy policy : policies) {
+
+      // Car policy
       if (policy.getPolicyType() == PolicyType.CAR) {
 
+        // Casts the policy to a CarPolicy object
         CarPolicy carPolicy = (CarPolicy) policy;
 
+        // Prints the details of the car policy
         MessageCli.PRINT_DB_CAR_POLICY.printMessage(
             carPolicy.getMakeAndModel(),
             String.valueOf(carPolicy.getSumInsured()),
             String.valueOf(carPolicy.getBasePremium()),
             String.valueOf(carPolicy.getDiscountedPremium(getNumPolicies())));
 
+        // Home policy
       } else if (policy.getPolicyType() == PolicyType.HOME) {
 
+        // Casts the policy to a HomePolicy object
         HomePolicy homePolicy = (HomePolicy) policy;
 
+        // Prints the details of the home policy
         MessageCli.PRINT_DB_HOME_POLICY.printMessage(
             homePolicy.getAddress(),
             String.valueOf(homePolicy.getSumInsured()),
             String.valueOf(homePolicy.getBasePremium()),
             String.valueOf(homePolicy.getDiscountedPremium(getNumPolicies())));
 
+        // Life policy
       } else {
 
+        // Casts the policy to a LifePolicy object
         LifePolicy lifePolicy = (LifePolicy) policy;
 
+        // Prints the details of the life policy
         MessageCli.PRINT_DB_LIFE_POLICY.printMessage(
-          String.valueOf(lifePolicy.getSumInsured()),
-          String.valueOf(lifePolicy.getBasePremium()),
-          String.valueOf(lifePolicy.getDiscountedPremium(getNumPolicies())));
-          
+            String.valueOf(lifePolicy.getSumInsured()),
+            String.valueOf(lifePolicy.getBasePremium()),
+            String.valueOf(lifePolicy.getDiscountedPremium(getNumPolicies())));
       }
     }
   }
