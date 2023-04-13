06428c7ddd94c22cd6a0f06490345a5f45b1b6fb
Jessica Jiang <jessicaj2947@gmail.com>
2023-04-13 14:06:17 +1200

    added comments and debugged

--- src/main/java/nz/ac/auckland/se281/LifePolicy.java
+++ src/main/java/nz/ac/auckland/se281/LifePolicy.java
@@ -1,20 +1,24 @@
 package nz.ac.auckland.se281;
+
 import nz.ac.auckland.se281.Main.PolicyType;
 
+// LifePolicy class
 public class LifePolicy extends Policy {
 
   private int age;
 
+  // Constructor
   public LifePolicy(String[] options, int age) {
     super(PolicyType.LIFE, options);
     this.age = age;
   }
 
+  // Implements the abstract method getBasePremium
   @Override
   public int getBasePremium() {
-    float Age = (float)(age);
-    float basePremium = ((1+Age/100)/100)*sumInusred;
-    int premium = Math.round(basePremium);
+    float Age = (float) (age);
+    float basePremium = ((1 + Age / 100) / 100) * sumInusred;
+    int premium = (int) (basePremium);
     return premium;
   }
 }
